c95a30ad9e43e189873dcbc1a39b3fd5df9fb66a
Oleg Zalozhenkov <oleg.zalozhenkov@gmail.com>
2024-12-21 22:34:24 +0100

    163. Install Redux Devtools Extension

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,4 +1,5 @@
 import { createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 const counterReducer = (state = { value: 0 }, action) => {
   switch (action.type) {
@@ -9,14 +10,13 @@ const counterReducer = (state = { value: 0 }, action) => {
   }
 };
 
-const store = createStore(counterReducer, { value: 0 });
+const store = createStore(counterReducer, { value: 0 }, composeWithDevTools());
 
 store.dispatch({
   type: "ADD",
   someValue: 10,
 });
 
-console.log(store.getState());
 
 export default store;
 

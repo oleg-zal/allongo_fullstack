4214da5bf79ce238dbadd6c56b948c4462a55d29
Oleg Zalozhenkov <oleg.zalozhenkov@gmail.com>
2024-12-24 08:21:05 +0100

    191. Info Messages when Adding To The Cart

--- frontend/src/pages/components/ProductDetailsPageComponent.js
+++ frontend/src/pages/components/ProductDetailsPageComponent.js
@@ -24,9 +24,11 @@ const ProductDetailsPageComponent = ({ addToCartReduxAction, reduxDispatch }) =>
 
     const {id} = useParams()
     const [quantity, setQuantity] = useState(1);
+    const [showCartMessage, setShowCartMessage] = useState(false);
 
     const addToCartHandler = () => {
         reduxDispatch(addToCartReduxAction(id, quantity));
+        setShowCartMessage(true);
     }
 
   var options = {
@@ -45,7 +47,7 @@ const ProductDetailsPageComponent = ({ addToCartReduxAction, reduxDispatch }) =>
   });
   return (
     <Container>
-      <AddedToCartMessageComponent />
+      <AddedToCartMessageComponent showCartMessage={showCartMessage} setShowCartMessage={setShowCartMessage} />
       <Row className="mt-5">
         <Col style={{ zIndex: 1 }} md={4}>
           <div id="first">

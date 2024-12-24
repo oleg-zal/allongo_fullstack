561f778e17a189ec1cce67043a0968d37b4ea142
Oleg Zalozhenkov <oleg.zalozhenkov@gmail.com>
2024-12-24 19:38:41 +0100

    207. List All Cart Items On User Order Details Page

--- frontend/src/pages/user/components/UserOrderDetailsPageComponent.js
+++ frontend/src/pages/user/components/UserOrderDetailsPageComponent.js
@@ -94,8 +94,8 @@ const UserOrderDetailsPageComponent = ({ userInfo, getUser, getOrder }) => {
           <br />
           <h2>Order items</h2>
           <ListGroup variant="flush">
-            {Array.from({ length: 3 }).map((item, idx) => (
-              <CartItemComponent item={{image: {path:"/images/tablets-category.png"}, name: "Product name", price:10, count:10, quantity:10}} key={idx} />
+            {cartItems.map((item, idx) => (
+              <CartItemComponent item={item} key={idx} orderCreated={true} />
             ))}
           </ListGroup>
         </Col>

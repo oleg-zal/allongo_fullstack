8a2b8eca1246936b6ee6959a962bb267ec2672af
Oleg Zalozhenkov <oleg.zalozhenkov@gmail.com>
2024-12-27 09:03:02 +0100

    255. Get Product Details

--- frontend/src/pages/ProductDetailsPage.js
+++ frontend/src/pages/ProductDetailsPage.js
@@ -5,13 +5,23 @@ import { useDispatch } from "react-redux";
 
 
 import { addToCart } from "../redux/actions/cartActions";
+import axios from 'axios'
+
+const getProductDetails = async(id) => {
+    const { data } = await axios.get(`/api/products/get-one/${id}`);
+    return data
+}
 
 const ProductDetailsPage = () => {
 
     const dispatch = useDispatch()
 
 
-  return <ProductDetailsPageComponent  addToCartReduxAction={addToCart} reduxDispatch={dispatch} />;
+  return <ProductDetailsPageComponent
+      addToCartReduxAction={addToCart}
+      reduxDispatch={dispatch}
+      getProductDetails={getProductDetails}
+  />;
 };
 
 export default ProductDetailsPage;

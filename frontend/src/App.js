b4e48310e7cc78b21b895761cd7231b80f2dcff7
Oleg Zalozhenkov <oleg.zalozhenkov@gmail.com>
2024-12-27 12:27:34 +0100

    262. Modify Home Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -44,7 +44,7 @@ function App() {
         <Route element={<RoutesWithUserChatComponent />}>
           {/* publicly available routes: */}
           <Route path="/" element={<HomePage />} />
-          <Route path="/product-list" element={<ProductListPage />} />
+          <Route path="/product-list/category/:categoryName" element={<ProductListPage />} />
           <Route path="/product-details/:id" element={<ProductDetailsPage />} />
           <Route path="/cart" element={<CartPage />} />
           <Route path="/login" element={<LoginPage />} />

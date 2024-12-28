5b56df934d9d9f8b57e7b8b2c0eb710e5304a076
Oleg Zalozhenkov <oleg.zalozhenkov@gmail.com>
2024-12-28 09:34:52 +0100

    301. Send Message from Admin To Client

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,10 @@ io.on("connection", (socket) => {
            message: msg, 
         })
     })
+
+    socket.on("admin sends message", ({ message }) => {
+        socket.broadcast.emit("server sends message from admin to client", message);
+    })
 })
 
 const apiRoutes = require("./routes/apiRoutes");
